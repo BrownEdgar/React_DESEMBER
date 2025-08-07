36a203416364fce6e81a387654a86129b863cd91
IDSHAO-777 <ashot.grigoryan.92.00@mail.ru>
2025-08-07 20:44:53 +0400

    Add homeworck

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './users/Ashot/Task1/App'
 
 createRoot(document.getElementById('root')).render(<App />)
---
+++ src/users/Ashot/Task1/App.jsx
@@ -0,0 +1,22 @@
+import   "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+    <div className="App__box1">
+    <h1>WEB DESGIN</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem earum ut possimus nemo blanditiis, rerum distinctio totam itaque atque iusto nam ratione ab est optio.</p>
+    </div>
+    <div className="App__box2">
+    <h1>WEB DESGIN</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem earum ut possimus nemo blanditiis, rerum distinctio totam itaque atque iusto nam ratione ab est optio.</p>
+    </div>
+    <div className="App__box3">
+    <h1>WEB DESGIN</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem earum ut possimus nemo blanditiis, rerum distinctio totam itaque atque iusto nam ratione ab est optio.</p>
+    </div>
+    </div>
+  )
+}
+
+export default App
