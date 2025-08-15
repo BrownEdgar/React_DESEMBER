ba34802c007e14a50fd47b0bf48203f4ebf0036c
HovhannesJeyranyan <jeyranyanhovhannes@gmail.com>
2025-08-16 00:15:27 +0400

    home worck 16.08.2024

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './users/Hovhannes/TASCKS/tasck3/App'
+import App from './users/Hovhannes/TASCKS/tasck4/App'
 
 createRoot(document.getElementById('root')).render(<App />)
---
+++ src/users/Hovhannes/TASCKS/tasck4/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react"
+import "./App.scss"
+
+
+export default function App(){
+
+const emailArr = [
+  {
+    id: 1,
+    fName: "Mark",
+    lName: "Otto",
+    email: "markotto@email.com"
+  },
+  {
+    id: 2,
+    fName: "Jacob",
+    lName: "Tharmont",
+    email: "jacobtharmont@email.com"
+  },
+  {
+    id: 3,
+    fName: "Larry",
+    lName: "the Bird",
+    email: "larrybird@email.com"
+  },
+  {
+    id: 4,
+    fName: "John",
+    lName: "Doe",
+    email: "Johndoe@email.com"
+  }
+]
+  
+const [arr, setArr] = useState(emailArr)
+
+return <div className="App">
+
+      <table>
+        <thead>
+              <tr>
+                <th className="head">#</th>
+                <th className="head">First Name</th>
+                <th>Last Name</th>
+                <th>Email Address</th>
+              </tr>
+            </thead>
+        <tbody>
+            {arr.map(elem => {
+
+              return <tr key={elem.id}>
+                <td>{elem.id}</td>
+                <td>{elem.fName}</td>
+                <td>{elem.lName}</td>
+                <td>{elem.email}</td>
+              </tr>
+            })}
+          
+        </tbody>
+      </table>
+
+  </div>
+
+}
