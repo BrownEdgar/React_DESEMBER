c7462c81072fe4f3587b3bb87840818f17fdaaf1
IDSHAO-777 <ashot.grigoryan.92.00@mail.ru>
2025-08-16 19:06:58 +0400

    k

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './users/Hovhannes/TASCKS/tasck4/App'
+import App from './users/Ashot/Task1/App'
 
 createRoot(document.getElementById('root')).render(<App />)
