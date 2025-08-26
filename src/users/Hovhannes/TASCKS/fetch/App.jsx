ddcae9fd759e41bfa990d86c8ef3da9e98594dc4
BrownEdgar <43911617+BrownEdgar@users.noreply.github.com>
2025-08-26 19:07:59 +0400

    fix code

--- src/users/Hovhannes/TASCKS/fetch/App.jsx
+++ src/users/Hovhannes/TASCKS/fetch/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer} from "react"
+import { useEffect, useReducer } from "react"
 import { initialState, reducer, SAVE_ITEMS, SET_ERROR, SET_LOADING } from "./reducer";
 import List from "./List"
 import "./App.scss"
@@ -6,26 +6,23 @@ import "./App.scss"
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  
+
   useEffect(() => {
-    const fetchData = async() => {
-      dispatch({type: SET_LOADING, payload: true})
-    try {
-      const response = await fetch("https://jsonplaceholder.typicode.com/todos")
-      const data = await response.json()
-      dispatch({type: SAVE_ITEMS, payload: data})
-    } catch (error) {
-      dispatch({type: SET_ERROR, payload: error})
-    }  
-      
-
-
-     }
-     fetchData()
+    const fetchData = async () => {
+      dispatch({ type: SET_LOADING, payload: true })
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+        const data = await response.json()
+        dispatch({ type: SAVE_ITEMS, payload: data })
+      } catch (error) {
+        dispatch({ type: SET_ERROR, payload: error })
+      }
+    }
+    fetchData()
   }, []);
-  
+
   return <div className="App">
-    {state.loading ? <h1>LOADING...</h1> : <List data={state.data} />} 
+    {state.loading ? <h1>LOADING...</h1> : <List data={state.data} />}
   </div>;
 
 }
--- src/users/Hovhannes/TASCKS/fetch/List.jsx
+++ src/users/Hovhannes/TASCKS/fetch/List.jsx
@@ -1,34 +1,23 @@
-import React from "react";
 import "./List.scss"
 
-
-export default function List({data }) {
-  function greenRed({data}){
+function greenRed({ data }) {
   const green = []
   const red = []
   data.map((elem) => {
     elem.completed ? green.push(elem) : red.push(elem);
   })
-  return [green, red]
+  return [...green, ...red]
 }
-const nData = greenRed({data})
-//console.log(greenRed({data}));
+
+export default function List({ data }) {
+  const nData = greenRed({ data })
+
   return <div className="List">
-    {nData[0].map((elem) => {
+    {nData.map((elem) => {
       const cl = `"List__item" ${elem.completed
         ? "List__item_green"
         : "List__item_red"}`;
-      return(
-        <div key={elem.id} className={cl}>
-          <h3>{elem.title}</h3>
-        </div>
-      )
-    })}
-    {nData[1].map((elem) => {
-      const cl = `"List__item" ${elem.completed
-        ? "List__item_green"
-        : "List__item_red"}`;
-      return(
+      return (
         <div key={elem.id} className={cl}>
           <h3>{elem.title}</h3>
         </div>
