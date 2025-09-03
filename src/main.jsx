0ce212ec02f5acb504d24ac232c802633a72ffc0
HovhannesJeyranyan <jeyranyanhovhannes@gmail.com>
2025-09-04 01:56:23 +0400

    home worck 02.09.25

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './users/Hovhannes/TASCKS/fetch/App'
+import App from './users/Hovhannes/TASCKS/hWorck02_09/App'
 
 createRoot(document.getElementById('root')).render(<App />)
---
+++ src/users/Hovhannes/TASCKS/hWorck02_09/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import "./App.scss"
+
+
+const abc = ["A", "B", "C", "D"]
+export default function App(){
+  const [letrArr, setLetrArr] = useState("...");
+
+  return <div className="App">
+    <div className="App__letDiv">
+      {abc.map(elem => {
+        return <div className={letrArr === elem ? "App__letDiv__activ" : "App__letDiv__upLeter"} key={elem} onClick={() => {setLetrArr(elem)}}>{elem}</div>
+      })}
+    </div>
+    
+    <div className="App__let">{letrArr}</div>
+  </div>
+  
+}
