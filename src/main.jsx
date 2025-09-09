8584015e00c3d911a9e8202935848b56ab4237a1
BrownEdgar <43911617+BrownEdgar@users.noreply.github.com>
2025-09-09 19:43:32 +0400

    cars example added

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './components/FORM/App'
+import 'primeicons/primeicons.css';
+import { createRoot } from 'react-dom/client'
+import App from './components/FORM/Cars/App'
 
 createRoot(document.getElementById('root')).render(<App />)
